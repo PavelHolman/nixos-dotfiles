a99bb40c279170aabf8cf5bbcf57a34e8cc1d2b7
Pavel Holman <pavelholman@email.cz>
2024-07-28 12:44:33 +0200

    terminal nix indent

--- terminal.nix
+++ terminal.nix
@@ -1,47 +1,49 @@
 { pkgs, ... }:
 
 {
-  programs.kitty = {
-    enable = true;
-    font = {
+  programs = {
+    kitty = {
+      enable = true;
+      font = {
         name = "JetBrainsMono Nerd Font";
         size = 10;
+      };
+      shellIntegration.enableZshIntegration = true;
+      theme = "Catppuccin-Mocha";
     };
-    shellIntegration.enableZshIntegration = true;
-    theme = "Catppuccin-Mocha";
-  };
-  programs.zsh = {
-    enable = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-    oh-my-zsh = {
+    zsh = {
       enable = true;
-      plugins = [
-        "git"
-        "rust"
-        "aliases"
-        "docker"
-      ];
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [
+          "git"
+          "rust"
+          "aliases"
+          "docker"
+        ];
+      };
+      shellAliases = {
+        gll = "git log --graph --abbrev-commit --decorate --all --date=format:'%Y-%m-%d %H:%M:%S' --format=format:'%C(bold blue)%h%C(reset) on %C(bold cyan)%ad%C(reset) %C(bold green)(%ar)%C(reset) %C(dim white)by %an%C(reset) %C(auto)%d%C(reset) %n%C(white)%s%C(reset)%n'";
+      };
     };
-    shellAliases = {
-      gll = "git log --graph --abbrev-commit --decorate --all --date=format:'%Y-%m-%d %H:%M:%S' --format=format:'%C(bold blue)%h%C(reset) on %C(bold cyan)%ad%C(reset) %C(bold green)(%ar)%C(reset) %C(dim white)by %an%C(reset) %C(auto)%d%C(reset) %n%C(white)%s%C(reset)%n'";
+    starship = {
+      enable = true;
+      settings = pkgs.lib.importTOML ./starship.toml;
     };
-  };
-  programs.starship = {
-    enable = true;
-    settings = pkgs.lib.importTOML ./starship.toml;
-  };
-  programs.fzf = {
-    enable = true;
-  };
-  programs.zoxide = {
-    enable = true;
-    options = [ "--cmd cd" ];
-  };
-  programs.eza = {
-    enable = true;
-  };
-  programs.ripgrep = {
-    enable = true;
+    fzf = {
+      enable = true;
+    };
+    zoxide = {
+      enable = true;
+      options = [ "--cmd cd" ];
+    };
+    eza = {
+      enable = true;
+    };
+    ripgrep = {
+      enable = true;
+    };
   };
 }
